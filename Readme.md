353b765301d1c2b3e4fe27b634f0ecb4b76955f8
Tomasz DUDEK <isn.dudek.tomasz@gmail.com>
2020-04-28 15:26:08 +0200

    Debut liste page similitude élevé

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Groupe 3 Algo du text
+
+# Membres
+
+- Haseeb JAVAID
+- Mathieu JUGI
+- Tomasz DUDEK
--- model/distancePage.py
+++ model/distancePage.py
@@ -1,4 +1,4 @@
-
+from progress.bar import Bar
 
 #Calcule la distance de hamming entre 2 string
 def dist_hamming(m1,m2):
@@ -8,9 +8,24 @@ def dist_hamming(m1,m2):
             d += 1
     return d
 
-#Parcour l'index et calcule la distance de hamming de 2 voisins 
+#Parcour l'index et calcule la distance de hamming de 2 voisins
+#Si la distance de hamming est trop petite on le met dans une liste a part
 def distance_pages (index):
-    #for i in range(int(len(index))-1):
-    for i in range(50):
+    #on initialise une liste vide pou stocker les pages trop similaires
+    list_same = []
+    #Compteur page trop similaires
+    cmpt = 0
+    #notre bar de progression
+    bar = Bar('Analyse', max=len(index))
+    for i in range(int(len(index))-1):
+    # for i in range(0,50):
+        bar.next()
         if dist_hamming(index[i][0], index[i+1][0]) < 50 :
-            print("Pour " + str(i) + " et " + str(i+1) + " on a distance de H : " + str(dist_hamming(index[i][1], index[i+1][1])))
+            #print("Pour " + str(i) + " et " + str(i+1) + " on a distance de H : " + str(dist_hamming(index[i][1], index[i+1][1])))
+            if dist_hamming(index[i][1], index[i+1][1]) < 20 :
+                cmpt += 2
+                list_same.append(index[i])
+                list_same.append(index[i+1])
+    bar.finish()
+    print("On a un ratio :" + str(cmpt) +"/"+ str(i+1))
+    return list_same
