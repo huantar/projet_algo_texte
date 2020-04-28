426c07459f82caad24fbf5998899689d0dc6ad6f
Tomasz DUDEK <isn.dudek.tomasz@gmail.com>
2020-04-28 14:09:21 +0200

    ajout parcours index pour dist hamming entre voisins

--- model/distancePage.py
+++ model/distancePage.py
@@ -1,6 +1,6 @@
 
 
-
+#Calcule la distance de hamming entre 2 string
 def dist_hamming(m1,m2):
     d = 0
     for a,b in zip(m1,m2):
@@ -8,13 +8,9 @@ def dist_hamming(m1,m2):
             d += 1
     return d
 
-
-def parcours_page (p1, p2):
-    print(dist_hamming(p1, p2))
-    #return dist_hamming(p1, p2))
-
-
-
-#for word1 in str.split(p1) :
-#    for word2 in str.split(p2) :
-#        dist_hamming()
+#Parcour l'index et calcule la distance de hamming de 2 voisins 
+def distance_pages (index):
+    #for i in range(int(len(index))-1):
+    for i in range(50):
+        if dist_hamming(index[i][0], index[i+1][0]) < 50 :
+            print("Pour " + str(i) + " et " + str(i+1) + " on a distance de H : " + str(dist_hamming(index[i][1], index[i+1][1])))
--- model/main.py
+++ model/main.py
@@ -3,7 +3,8 @@ from distancePage import *
 
 # repertoire="/home/hasib/algoTexte/pages_web"
 repertoire="D:\\Users\\Tomasz\\Documents\\mes_doc\\master\\data\\pages_web"
+print("Chargement Index \n")
 index = loadIndex(repertoire)
 print("Chargement Index fini \n")
 #print(index[0])
-parcours_page(index[1][1], index[2][1])
+distance_pages(index)
