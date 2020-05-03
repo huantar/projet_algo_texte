476c3780b8df35a0dc986f1382c908377e2e06b1
hasibo <39988600+hasibo@users.noreply.github.com>
2020-05-03 18:30:53 +0200

    Update calculScore.py

--- model/calculScore.py
+++ model/calculScore.py
@@ -1,28 +1,28 @@
-#prend en paramère le contenu split d'une page
+#prend en paramère le contenu split d'une requête
 #renvoie le contenu split sans les doublons
-def enleveDoublons(document):
+def enleveDoublons(requete):
     #on enlève les doublons
-    dictionnaireMots=list(dict.fromkeys(document))
+    dictionnaireMots=list(dict.fromkeys(requete))
     return dictionnaireMots
 
-#prend en paramètre le contenu split d'une page 
+#prend en paramètre le contenu split d'une requête
 #retourne un tableau contenant les mots et leur TF
-def calculTF(document):
+def calculTF(requete):
     TF=[]
-    dictionnaireMots=enleveDoublons(document)
-    nbrTotalMots=len(document)
+    dictionnaireMots=enleveDoublons(requete)
+    nbrTotalMots=len(requete)
     for i in range(len(dictionnaireMots)):
-        tfValeur=document.count(dictionnaireMots[i])/nbrTotalMots
+        tfValeur=requete.count(dictionnaireMots[i])/nbrTotalMots
         TF.append([dictionnaireMots[i],tfValeur])
     return TF
 
-#le contenu split d'une page et l'index qui a son contenu splité
+#le contenu split d'une requête et l'index qui a son contenu splité
 #retourne un tableau contenant les mots et leur IDF
-def calculIDF(document,listeDocuments):
+def calculIDF(requete,listeDocuments):
     import math
     IDF=[]
     nbrDocuments=len(listeDocuments)
-    dictionnaireMots=enleveDoublons(document)
+    dictionnaireMots=enleveDoublons(requete)
     for k in range(len(dictionnaireMots)):
         occurParDoc=0
         for j in range(len(listeDocuments)):
@@ -32,14 +32,14 @@ def calculIDF(document,listeDocuments):
         IDF.append([dictionnaireMots[k],idfValeur])
     return IDF
 
-    #prend en paramètre le tableau des TF et celui des IDF
-    #retourne un tableau contenant les mots et leurs TF-IDF
-    def calculTFIDF(tf,idf):
-        TFIDF=[]
-        for i in range(len(tf)):
-            tfidfValeur=tf[i][1]*idf[i][1]
-            TFIDF.append([tf[i][0],tfidfValeur])
-        return TFIDF
+#prend en paramètre le tableau des TF et celui des IDF
+#retourne un tableau contenant les mots et leurs TF-IDF
+def calculTFIDF(tf,idf):
+    TFIDF=[]
+    for i in range(len(tf)):
+        tfidfValeur=tf[i][1]*idf[i][1]
+        TFIDF.append([tf[i][0],tfidfValeur])
+    return TFIDF
 
 #prend en paramètre l'index
 #retourne le avgdl(longueur moyenne des documents)
@@ -51,24 +51,21 @@ def calculAVGDL(listeDocuments):
     avgdl=sommeMots/nombreDocuments
     return avgdl
 
-#prend en paramètre l'index qui a son contenu splité
-#retourne le score bm-25 toutes les pages de l'index dans un tableau trié selon le score
-def calculScore25(listeDocuments):
+#prend en paramètre l'index qui a son contenu splité et la requete splité
+#retourne le score bm-25 de la requete pour chaque url
+def calculScore25(requete,listeDocuments):
     from operator import itemgetter
     score=[]
-    for j in range(len(listeDocuments)):
-        document=listeDocuments[j][1]
-        score25=0
-        idf=calculIDF(document,listeDocuments)
-        avgdl=calculAVGDL(listeDocuments)
-        k=2.0
-        b=0.75
-        #on calcul le score pour chaque document
+    score25=0
+    idf=calculIDF(requete,listeDocuments)
+    avgdl=calculAVGDL(listeDocuments)
+    k=2.0
+    b=0.75
+    for j in range(len(listeDocuments)):   
         for i in range(len(idf)):
-            dividende=idf[i][1]*(document.count(idf[i][0])*(k+1))
-            diviseur=document.count(idf[i][0])+k*((1-b)+b*(len(document)/avgdl))
+            dividende=idf[i][1]*(listeDocuments[j][1].count(idf[i][0])*(k+1))
+            diviseur=listeDocuments[j][1].count(idf[i][0])+k*((1-b)+b*(len(listeDocuments[j][1])/avgdl))
             score25=score25+(dividende/diviseur)
         score.append([score25,listeDocuments[j][0]])
-    #on trie le tableau des score
-    score=sorted(score, key=itemgetter(0))
+    score=sorted(score, key=itemgetter(0),reverse=True)
     return score    
