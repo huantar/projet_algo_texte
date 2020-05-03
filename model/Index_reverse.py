721d1dca73083efc3802fd178c9b0b3b72cec274
hasibo <39988600+hasibo@users.noreply.github.com>
2020-05-03 15:47:13 +0200

    Update Index_reverse.py

--- model/Index_reverse.py
+++ model/Index_reverse.py
@@ -1,3 +1,5 @@
+from calculScore import *
+
 class Index_reverse :
     """ Répertorie les mots de l'index selon les pages où ils se trouvent et leur occurence dans ces pages.
         Cette classe à pour paramètres :
@@ -15,10 +17,16 @@ class Index_reverse :
             mots.extend(list(dict.fromkeys(index[i][1])))
         #on s'assure d'enlever les mots qui reviennent dans plusieurs pages
         mots=list(dict.fromkeys(mots))
-        #on définit reverse tel que [[mots1,[nbrOcc,url1],...,[nbrOcc,url2]],....,[motsN,[nbrOcc,url1],...,[nbrOcc,url2]]]
+        #on définit reverse tel que [[mots1,[score,url1],...,[score,url2]],....,[motsN,[score,url1],...,[score,url2]]]
         for i in range(len(mots)):
-            self.reverse.append([mots[i]])
+            self.reverse.append([mots[i],[]])
             for j in range(len(index)):
-                #si le mots est dans la page on ajoute [nbrOccurence,url] à reverse
-                if index[j][1].count(mots[i])>0:
-                    self.reverse[i].append([index[j][1].count(mots[i]),index[j][0]])
+                if mots[i] in index[j][1]:
+                    #on ajoute [url,page] à reverse
+                    self.reverse[i][1].append([index[j][0],index[j][1]])
+                    #supprimer le mots de la page
+                    index[j][1] = list(filter(lambda x: x != mots[i], index[j][1]))
+        #on remplace le [url,page] par [score,url] 
+        for k in range(len(self.reverse)):
+            self.reverse[k][1]=calculScore25(self.reverse[k][1])
+                
