7f7110d2fa85cbd720a15261d4287bde5a83cd01
Tomasz DUDEK <isn.dudek.tomasz@gmail.com>
2020-05-03 17:44:00 +0200

    update distancePage

--- model/distancePage.py
+++ model/distancePage.py
@@ -1,8 +1,34 @@
+# Nousa vons decide d'utiliser la distance de hamming,
+# car aprés plusieurs test c'etait l'algo le plus rapide
 
-#Calcule la distance de hamming entre 2 string
+######## hamming ########
 def dist_hamming(m1,m2):
     d = 0
     for a,b in zip(m1,m2):
         if a != b :
             d += 1
     return d
+
+######## levenshtein ########
+def lev(a, b):
+    if not a: return len(b)
+    if not b: return len(a)
+    return min(lev(a[1:], b[1:])+(a[0] != b[0]), lev(a[1:], b)+1, lev(a, b[1:])+1)
+
+def levenshtein(s, t):
+        if s == t: return 0
+        elif len(s) == 0: return len(t)
+        elif len(t) == 0: return len(s)
+        v0 = [None] * (len(t) + 1)
+        v1 = [None] * (len(t) + 1)
+        for i in range(len(v0)):
+            v0[i] = i
+        for i in range(len(s)):
+            v1[0] = i + 1
+            for j in range(len(t)):
+                cost = 0 if s[i] == t[j] else 1
+                v1[j + 1] = min(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost)
+            for j in range(len(v0)):
+                v0[j] = v1[j]
+
+        return v1[len(t)]
