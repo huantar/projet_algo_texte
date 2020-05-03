7881492fbd4f1a390bae0c5c7aa3ba95125406a9
Tomasz DUDEK <isn.dudek.tomasz@gmail.com>
2020-05-04 00:28:12 +0200

    push main timer

--- model/main.py
+++ model/main.py
@@ -1,12 +1,14 @@
 from loadIndex import *
 from Index_reverse import *
-
+import time
 
 
 # repertoire="/home/hasib/algoTexte/pages_web"
-repertoire="D:\\Users\\Tomasz\\Documents\\mes_doc\\master\\data\\pages_web500"
+repertoire="D:\\Users\\Tomasz\\Documents\\mes_doc\\master\\data\\pages_web"
 print("Chargement Index a partir du dossier : " + repertoire + "\n")
 data = Data(repertoire)
 # data.find_word('2020')
+start = time.time()
 index_inverse = Index_reverse(data.index)
-print(index_inverse.reverse[0])
+# print(index_inverse.reverse[0])
+print("temps prit pour le reverse  :" + str((time.time()-start)/60) + " min \n")
