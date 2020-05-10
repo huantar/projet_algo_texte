152548103fe9ab770b007c24f07c07d6cd1a1df3
Tomasz DUDEK <isn.dudek.tomasz@gmail.com>
2020-05-10 13:35:03 +0200

    Ajout ancienne recherche proches 1 mot dans index

--- model/loadIndex.py
+++ model/loadIndex.py
@@ -66,9 +66,34 @@ class Data :
                         self.index[tabPage[j]][1] += 1
         bar.finish()
 
-    # Supprime les pages trop similaires pour n'en avoir qu'une 
+    # Supprime les pages trop similaires pour n'en avoir qu'une
     def clean_index(self):
         print("Avant suppression on avait : " + str(len(self.index)) + " pages dans l'index")
         for url in self.list_same :
             self.index.pop(url)
         print("Aprés  suppression on a : " + str(len(self.index)) + " pages dans l'index")
+
+    # Function qui retrouve dans l'index les mots similaires a un mot clé
+    def find_single_word(self, word):
+        # On initialise notre tableau de retour
+        single_word_same=[]
+        #si on cherche un chiffre alors on ne fait pas hamming
+        if word.isdigit():
+            for page in self.index :
+                txt = page[1]
+                for mot in txt :
+                    if word == mot:
+                        word_same.append(mot)
+        #Sinon, on fait hamming
+        else:
+            # On parcours l'index
+            for page in self.index :
+                txt = page[1]
+                wt = len(word)
+                # Pour chaque page on parcours le contenu
+                for mot in txt :
+                    # Et on verifie avec hamming la distance entre les deux mots,
+                    # De plus on accepte les mots qui sont proche a plus ou moins une lettre
+                    if dist_hamming(mot,word) < wt/2 and wt-1 <= len(mot)  <= wt+1 :
+                        word_same.append(mot)
+        return single_word_same
