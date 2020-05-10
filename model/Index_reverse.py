e835a3ea81b7b3733d8a16a8412301f9e0908227
Tomasz DUDEK <isn.dudek.tomasz@gmail.com>
2020-05-10 15:49:03 +0200

    Ajout commentaires index_reverse

--- model/Index_reverse.py
+++ model/Index_reverse.py
@@ -9,21 +9,21 @@ class Index_reverse :
         - reverse (qui contient l'occurences des mots par pages)
         """
 
-     #constructeur de la classe index reserve
+     # Constructeur de la classe index reserve
     def __init__(self,index):
         self.reverse = {}
-        #on enleve les doublons du contenu de chaque page avec split et on met la liste renvoyé à mots
+        # On enleve les doublons du contenu de chaque page avec split et on met la liste renvoyé à mots
         for url, contenu in index.items():
             contenu[0] = contenu[0].split()
             mots = []
             mots.extend(list(dict.fromkeys(contenu[0])))
             for k in range(len(mots)):
                 if bool(self.reverse.get(mots[k])):
-                    # print(url)
                     self.reverse[mots[k]].append(url)
                 else:
                     self.reverse[mots[k]]=[url]
 
+    # Function qui return la liste des mots proches d'une liste de mots
     def find_word(self, requete):
         word_same=[]
         dictionnaireMots= list(self.reverse.keys())
@@ -49,37 +49,36 @@ class Index_reverse :
         return list(dict.fromkeys(word_same))
 
 
-        #prend en paramètre une requète et renvoie les 10 meilleurs pages correspondantes
+    # Prend en paramètre une requète et renvoie les 10 meilleurs pages correspondantes
     def recherche(self, requete, d):
+        # On recupere une liste de mots de la requete
         requete = requete.split()
-        #On recherche les mots proches
         mProches = []
-
         start = time.time()
+        #On recherche les mots proches
         mProches = self.find_word(requete)
         print("temps prit pour le findword:" + str((time.time()-start)/60) + " min \n")
 
-
         #liste de page (0:url et 1:contenue) ou notre mot apparait
         reverseContenu=[]
-        #on cherche les mots de la requete dans l'index inverse
+        # On cherche les mots de la requete dans l'index inverse
         for i in range(len(mProches)):
             if bool(self.reverse.get(mProches[i])):
                 #on évite de mettre 2 fois la même page pour le calcul
                 reverseContenu.extend(self.reverse[mProches[i]])
+        # On supprime les eventuels doublons
         reverseContenu = list(dict.fromkeys(reverseContenu))
-        #on remplace les url par le contenu des pages
+        # On remplace les urls par le contenu des pages
         for url in reverseContenu :
             if bool(d.index.get(url)):
                 reverseContenu[reverseContenu.index(url)] =[url,  d.index[url][0]]
 
         start = time.time()
-        #tableau des scores avec leurs urls triées
+        #On créer le tableau des scores avec leurs urls triées
         if len(reverseContenu) > 0:
             score=calculScore25(requete,reverseContenu)
-
         print("temps prit pour score:" + str((time.time()-start)/60) + " min \n")
-        # print(score)
+        
         bestPages=[]
         if not(reverseContenu):
             return bestPages
